78870782f6a4a43fe7dba6e3bcc422ddd876dd21
Unknown <notavailablenow@mail.ru>
2018-02-19 15:00:45 +0300

    HW 1.1 - v.1

---
+++ CSharpHW/HW1.1/HW1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace HW1_1
+{
+	class Program
+	{
+		static int Factorial(int n)
+		{
+			if (n <= 1)
+			{
+				return 1;
+			}
+			else
+			{
+				int factorial = 1;
+				for (int i = 1; i <= n; i++)
+				{
+					factorial *= i;
+				}
+				return factorial;
+			}
+		}
+		static void Main()
+		{
+			int number;
+			Console.WriteLine("Enter number to calculate factorial");
+			number = int.Parse(Console.ReadLine());
+			Console.WriteLine("Factorial of number "+ number + " is " + Factorial(number));
+		}
+	}
+}
